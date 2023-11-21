6027553e86ef7e85ba2f09061da9da8e97f4e0eb
Moisés <moises_arruda@outlook.com>
2023-11-20 21:13:19 -0300

    Pacote de carregamento funcionando

--- app/pipeline/load.py
+++ app/pipeline/load.py
@@ -1,6 +1,6 @@
 """Pacote de carregamento."""
 
-from app.pipeline.extract import verify_dat
+from extract import verify_dat,definir_caminhos
 
 def definindo_arquivos_dat():
     """
@@ -9,18 +9,27 @@ def definindo_arquivos_dat():
     METEO_1M = "CR6_T1_meteo_"
     # Lista para armazenar a quantidade de arquivos 1m no One Drive
     # Variável para armazenar parte do nome do arquivo
-    METEO_1M = "meteoMedia30"
+    METEO_30M = "meteoMedia30"
     quantidade_1m = []
     # Lista para armazenar a quantidade de arquivos 30m no One Drive
     quantidade_30m = []
 
+    origem, staging, destino = definir_caminhos()
+
+    # Chamando verify_dat para obter a lista de nomes de arquivos
+    nome_arquivos = verify_dat(origem)
+
     # Loop para iterar em cada arquivo na pasta
-    for arquivo in verify_dat(origem):
-        if METEO_1M in nome_arquivos:
-            quantidade_30m.append(nome_arquivos)
-        elif METEO_1M in nome_arquivos:
-            quantidade_1m.append(nome_arquivo) 
+    for arquivo in nome_arquivos:
+        if METEO_30M in arquivo:
+            quantidade_30m.append(arquivo)
+        elif METEO_1M in arquivo:
+            quantidade_1m.append(arquivo) 
         else:
             print('Nenhum arquivo na pasta')
 
-    return len(quantidade_30m), len(quantidade_1m)
+    return quantidade_30m, quantidade_1m
+
+resultados_30m, resultados_1m = definindo_arquivos_dat()
+print(f"Quantidade de arquivos 30m: {len(resultados_30m)}")
+print(f"Quantidade de arquivos 1m: {len(resultados_1m)}")
