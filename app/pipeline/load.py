e4fbe7ec2d6c06196c4f58be745fea4c3312e7e0
Moisés <moises_arruda@outlook.com>
2023-11-23 11:52:23 -0300

    Rodando isort . e blue . para formatação

--- app/pipeline/load.py
+++ app/pipeline/load.py
@@ -8,13 +8,14 @@ import os
 # Manipulação de arquivos e diretórios
 import shutil
 
-from extract import verify_dat,definindo_arquivos_dat,definir_caminhos
+from extract import definindo_arquivos_dat, definir_caminhos, verify_dat
 
 # Definindo constantes globais
 origem, staging, destino = definir_caminhos()
 
 # Modificar para obter os arquivos da pasta origem
 arquivos = verify_dat()
+
 
 def conferir_arquivos_nao_enviados():
     """
@@ -23,15 +24,16 @@ def conferir_arquivos_nao_enviados():
     """
 
     arquivo_1m_apenas_origem = []
-    
-    quantidade_30m,quantidade_1m = definindo_arquivos_dat()
-    
+
+    quantidade_30m, quantidade_1m = definindo_arquivos_dat()
 
     # Loop que vai iterar sobre os arquivos 1m
     for arquivo in quantidade_1m:
         # Apenas o nome do arquivo, que não está na pasta 1m do drive
-         if os.path.basename(arquivo) not in [os.path.basename(x) for x in destino]:
-         # Adicionar o nome do arquivo na lista
+        if os.path.basename(arquivo) not in [
+            os.path.basename(x) for x in destino
+        ]:
+            # Adicionar o nome do arquivo na lista
             arquivo_1m_apenas_origem.append(arquivo)
 
     # Retornar a quantidade de arquivo
@@ -59,13 +61,14 @@ def enviar_arquivos_para_staging():
         # Copiar o arquivo de uma pasta para outra
         shutil.copy(caminho_arquivo_origem, caminho_arquivo_staging)
         # Para cada arquivo 1 mensagem
-        print (f'Arquivo {arquivos} copiado para a pasta Staging')
+        print(f'Arquivo {arquivos} copiado para a pasta Staging')
         qntd_enviada.append(+1)
 
     return qntd_enviada
 
+
 try:
     qntd_enviada = enviar_arquivos_para_staging()
-    print(f"Foi realizado o envio de {len(qntd_enviada)} arquivos")
+    print(f'Foi realizado o envio de {len(qntd_enviada)} arquivos')
 except (FileNotFoundError, ValueError) as e:
     print(e)
