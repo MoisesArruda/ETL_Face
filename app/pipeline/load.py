f908c86a7393cdf52d5713af9f987d5024ed453f
Moisés <moises_arruda@outlook.com>
2023-11-23 11:46:02 -0300

    Melhoria no pacote de carregamento

--- app/pipeline/load.py
+++ app/pipeline/load.py
@@ -1,19 +1,30 @@
 """Pacote de carregamento."""
+# Acessa o sistema operacional
+import glob
 
-from extract import verify_dat,definir_caminhos
+# Irá retornar uma lista de arquivos
+import os
+
+# Manipulação de arquivos e diretórios
+import shutil
+
+from extract import verify_dat,definindo_arquivos_dat,definir_caminhos
 
 # Definindo constantes globais
-# ORIGEM, STAGING, DESTINO = definir_caminhos()
+origem, staging, destino = definir_caminhos()
 
+# Modificar para obter os arquivos da pasta origem
+arquivos = verify_dat()
 
-def conferir_arquivos_nao_enviados(origem,destino):
+def conferir_arquivos_nao_enviados():
     """
     Este trecho tem o objetivo de verificar quais arquivos ainda
     não foram enviados para a pasta destino
     """
+
     arquivo_1m_apenas_origem = []
     
-    quantidade_1m = definindo_arquivos_dat()
+    quantidade_30m,quantidade_1m = definindo_arquivos_dat()
     
 
     # Loop que vai iterar sobre os arquivos 1m
@@ -24,27 +35,37 @@ def conferir_arquivos_nao_enviados(origem,destino):
             arquivo_1m_apenas_origem.append(arquivo)
 
     # Retornar a quantidade de arquivo
-    print(f'Arquivos de 1 minuto {len(arquivo_1m_apenas_origem)}')
+    print(f'Arquivos de 1 minuto: {len(arquivo_1m_apenas_origem)}')
     return arquivo_1m_apenas_origem
 
-def enviar_arquivos_para_staging(origem,staging):
+
+arquivo_1m_apenas_origem = conferir_arquivos_nao_enviados()
+print(arquivo_1m_apenas_origem)
+
+
+def enviar_arquivos_para_staging():
     """
     Deve enviar os arquivos ainda não enviados da pasta Origem
-    para a pasta staging"""
-    
-    arquivo_1m_apenas_origem = conferir_arquivos_nao_enviados()
-    
+    para a pasta staging
+    """
+    qntd_enviada = []
+
     # Loop para iterar os arquivos 30m que estão apenas no One
-    for nome_arquivo in arquivo_1m_apenas_origem:
+    for arquivos in arquivo_1m_apenas_origem:
         # Buscar o caminho desse arquivo no One Drive
-        caminho_arquivo_origem = f'{ORIGEM}\\{nome_arquivo}'
+        caminho_arquivo_origem = os.path.join(origem, arquivos)
         # Buscar a pasta Staging no GDrive
-        caminho_arquivo_staging = f'{STAGING}\\{nome_arquivo}'
+        caminho_arquivo_staging = os.path.join(staging, arquivos)
         # Copiar o arquivo de uma pasta para outra
         shutil.copy(caminho_arquivo_origem, caminho_arquivo_staging)
         # Para cada arquivo 1 mensagem
-        print (f'Arquivo {nome_arquivo} copiado para a pasta Staging')
-        qntd_enviada += 1
+        print (f'Arquivo {arquivos} copiado para a pasta Staging')
+        qntd_enviada.append(+1)
 
-        print(len(qntd_enviada))
+    return qntd_enviada
 
+try:
+    qntd_enviada = enviar_arquivos_para_staging()
+    print(f"Foi realizado o envio de {len(qntd_enviada)} arquivos")
+except (FileNotFoundError, ValueError) as e:
+    print(e)
