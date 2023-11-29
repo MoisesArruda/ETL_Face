e15196d2a001a52733db0b1d12f1dd5026294cbf
Moisés <moises_arruda@outlook.com>
2023-11-29 13:50:07 -0300

    Atualizando os caminhos dos arquivos

--- app/pipeline/load.py
+++ app/pipeline/load.py
@@ -8,16 +8,16 @@ import os
 # Manipulação de arquivos e diretórios
 import shutil
 
-from extract import definindo_arquivos_dat, definir_caminhos, verify_dat
+from extract import definindo_arquivos_dat_1m, definindo_arquivos_dat_30m, definir_caminhos, verify_dat
 
 # Definindo constantes globais
 origem, staging, destino = definir_caminhos()
 
 # Modificar para obter os arquivos da pasta origem
-arquivos = verify_dat()
+arquivos = verify_dat(origem)
 
 
-def conferir_arquivos_nao_enviados():
+def conferir_arquivos_nao_enviados_1m():
     """
     Este trecho tem o objetivo de verificar quais arquivos ainda
     não foram enviados para a pasta destino
@@ -25,7 +25,7 @@ def conferir_arquivos_nao_enviados():
 
     arquivo_1m_apenas_origem = []
 
-    quantidade_30m, quantidade_1m = definindo_arquivos_dat()
+    quantidade_1m = definindo_arquivos_dat_1m()
 
     # Loop que vai iterar sobre os arquivos 1m
     for arquivo in quantidade_1m:
@@ -41,16 +41,43 @@ def conferir_arquivos_nao_enviados():
     return arquivo_1m_apenas_origem
 
 
-arquivo_1m_apenas_origem = conferir_arquivos_nao_enviados()
+arquivo_1m_apenas_origem = conferir_arquivos_nao_enviados_1m()
 print(arquivo_1m_apenas_origem)
 
 
-def enviar_arquivos_para_staging():
+def conferir_arquivos_nao_enviados_30m():
+    """
+    Este trecho tem o objetivo de verificar quais arquivos ainda
+    não foram enviados para a pasta destino
+    """
+
+    arquivo_30m_apenas_origem = []
+
+    quantidade_30m = definindo_arquivos_dat_30m()
+
+    # Loop que vai iterar sobre os arquivos 1m
+    for arquivo in quantidade_30m:
+        # Apenas o nome do arquivo, que não está na pasta 1m do drive
+        if os.path.basename(arquivo) not in [
+            os.path.basename(x) for x in destino
+        ]:
+            # Adicionar o nome do arquivo na lista
+            arquivo_30m_apenas_origem.append(arquivo)
+
+    # Retornar a quantidade de arquivo
+    print(f'Arquivos de 30 minutos: {len(arquivo_30m_apenas_origem)}')
+    return arquivo_30m_apenas_origem
+
+arquivo_30m_apenas_origem = conferir_arquivos_nao_enviados_30m()
+print(arquivo_30m_apenas_origem)
+
+
+def enviar_arquivos_1m_para_staging():
     """
     Deve enviar os arquivos ainda não enviados da pasta Origem
     para a pasta staging
     """
-    qntd_enviada = []
+    qntd_enviada_1m = []
 
     # Loop para iterar os arquivos 30m que estão apenas no One
     for arquivos in arquivo_1m_apenas_origem:
@@ -62,13 +89,13 @@ def enviar_arquivos_para_staging():
         shutil.copy(caminho_arquivo_origem, caminho_arquivo_staging)
         # Para cada arquivo 1 mensagem
         print(f'Arquivo {arquivos} copiado para a pasta Staging')
-        qntd_enviada.append(+1)
+        qntd_enviada_1m.append(+1)
 
-    return qntd_enviada
+    return qntd_enviada_1m
 
 
 try:
-    qntd_enviada = enviar_arquivos_para_staging()
+    qntd_enviada = enviar_arquivos_1m_para_staging()
     print(f'Foi realizado o envio de {len(qntd_enviada)} arquivos')
 except (FileNotFoundError, ValueError) as e:
     print(e)
